390f2f3c3be23d199069065a199f9b4b2f790698
Dustin Essington <aetaric@gmail.com>
2017-04-06 23:16:11 -0500

    moved massive setup block from bot.rb to lib/brain.rb. Moved non-opper message to USERSTATE.

--- bot.rb
+++ bot.rb
@@ -25,89 +25,7 @@ $live_chans = []
 $team_chans = []
 
 if !$brain.config
-  system "clear"
-  puts "Configuring Bot Info..."
-  puts ""
-  bot = {}
-  print "Bot Nickname: "
-  bot["nick"] = gets.chomp
-  print "IRC Server: "
-  bot["server"] = gets.chomp
-  print "IRC Port: "
-  bot["port"] = gets.chomp.to_i
-
-  print "Use SSL?[Y/n] "
-  response = gets.chomp.downcase
-  if response === "y"
-    bot["ssl"] = true
-  elsif response === "n"
-    bot["ssl"] = false
-  else
-    bot["ssl"] = true
-  end
-
-  print "Verify SSL?[y/N] "
-  response = gets.chomp.downcase
-  if response === "y"
-    bot["ssl_verify"] = true
-  elsif response === "n"
-    bot["ssl_verify"] = false
-  else
-    bot["ssl_verify"] = false
-  end
-  
-  print "Server Password?[default is none] "
-  response = gets.chomp
-  if response === ""
-    bot["password"] = gets.chomp
-  else
-    bot["password"] = ""
-  end
-
-  $brain.bot = bot
-  puts "Bot Info configured..."
-  sleep 1
-
-  puts "Configuring Admin User..."
-  users = []
-  user = {:permission => 75}
-  print "Enter nickname of Bot Owner: "
-  user["nick"] = gets.chomp
-  print "Enter ident(sso) of Bot Owner: "
-  user["ident"] = gets.chomp
-  users.push user
-  $brain.users = users
-  puts "Admin User configured..."
-  sleep 1
-
-  puts "Configuring Initial Channel..."
-  chans = []
-  channel = {}
-  print "Channel name: "
-  channel["name"] = gets.chomp
-  chans.push channel
-  $brain.channels = chans
-  puts "Initial Channel configured..."
-  sleep 1
-
-  puts "Setting plugins to defaults..."
-  plugins = [constantize("CommandPlugin"), constantize("Cinch::Logging"), constantize("Twitch")]
-  $brain.plugins = plugins
-
-  puts "Configuring Twitch Credentials..."
-  twitch = {}
-  print "Client ID: "
-  twitch["client"] = gets.chomp
-  print "Client Secret: "
-  twitch["secret"] = gets.chomp
-  print "Twitch Team: "
-  twitch["team"] = gets.chomp
-  $brain.twitch = twitch
-
-  system("clear")
-  puts "Initial config complete... SAVING!"
-  $brain.save
-  $brain.reload
+  $brain.setup
 end
 
 channels = []
@@ -119,8 +37,6 @@ plugins = []
 $brain.plugins.each do |plugin|
   plugins.push constantize(plugin)
 end
-
-
 
 @bot = Cinch::Bot.new do
   configure do |c|
@@ -160,12 +76,34 @@ end
   
   on :join do |m|
     if m.user == $brain.bot["nick"]
-      if !$brain.channels.include? m.channel
-        $brain.channels.push m.channel
+
+      match_chan = false
+
+      $brain.channels.each do |chan|
+        if !(chan["name"] == m.channel.to_s)
+          match_chan = true
+        end
+      end
+
+      if !match_chan
+        ch = {}
+        ch["name"] = m.channel.to_s
+        $brain.channels.push ch
         $brain.save
       end
+
       sleep 120
-      if !m.channel.opped? $brain.bot["nick"]
+
+      #if !m.channel.opped? $brain.bot["nick"] || !m.channel.to_s.split(1,m.channel.to_s.length) == $brain.bot["nick"]
+      #  m.reply "@" + chan_to_user(m) + ", I need Mod and Editor permissions in order to function! Please Mod me and add me to your Editors."
+      #end
+
+    end
+  end
+
+  on :userstate do |m|
+    if !mod?(m)
+      if !m.channel.to_s.split(1,m.channel.to_s.length) == $brain.bot["nick"].to_s
         m.reply "@" + chan_to_user(m) + ", I need Mod and Editor permissions in order to function! Please Mod me and add me to your Editors."
       end
     end
--- lib/brain.rb
+++ lib/brain.rb
@@ -42,4 +42,90 @@ class Brain
     @twitch = JSON.load(@redis.get("twitch"))
     @config = true
   end
+
+  def setup
+    system "clear"
+    puts "Configuring Bot Info..."
+    puts ""
+    bot = {}
+    print "Bot Nickname: "
+    bot["nick"] = gets.chomp
+    print "IRC Server: "
+    bot["server"] = gets.chomp
+    print "IRC Port: "
+    bot["port"] = gets.chomp.to_i
+
+    print "Use SSL?[Y/n] "
+    response = gets.chomp.downcase
+    if response === "y"
+      bot["ssl"] = true
+    elsif response === "n"
+      bot["ssl"] = false
+    else
+      bot["ssl"] = true
+    end
+
+    print "Verify SSL?[y/N] "
+    response = gets.chomp.downcase
+    if response === "y"
+      bot["ssl_verify"] = true
+    elsif response === "n"
+      bot["ssl_verify"] = false
+    else
+      bot["ssl_verify"] = false
+    end
+    
+    print "Server Password?[default is none] "
+    response = gets.chomp
+    if response === ""
+      bot["password"] = gets.chomp
+    else
+      bot["password"] = ""
+    end
+
+    $brain.bot = bot
+    puts "Bot Info configured..."
+    sleep 1
+
+    puts "Configuring Admin User..."
+    users = []
+    user = {:permission => 75}
+    print "Enter nickname of Bot Owner: "
+    user["nick"] = gets.chomp
+    print "Enter ident(sso) of Bot Owner: "
+    user["ident"] = gets.chomp
+    users.push user
+    $brain.users = users
+    puts "Admin User configured..."
+    sleep 1
+
+    puts "Configuring Initial Channel..."
+    chans = []
+    channel = {}
+    print "Channel name: "
+    channel["name"] = gets.chomp
+    chans.push channel
+    $brain.channels = chans
+    puts "Initial Channel configured..."
+    sleep 1
+
+    puts "Setting plugins to defaults..."
+    plugins = [constantize("CommandPlugin"), constantize("Cinch::Logging"), constantize("Twitch")]
+    $brain.plugins = plugins
+
+    puts "Configuring Twitch Credentials..."
+    twitch = {}
+    print "Client ID: "
+    twitch["client"] = gets.chomp
+    print "Client Secret: "
+    twitch["secret"] = gets.chomp
+    print "Twitch Team: "
+    twitch["team"] = gets.chomp
+    $brain.twitch = twitch
+
+    system("clear")
+    puts "Initial config complete... SAVING!"
+    $brain.save
+    $brain.reload
+  end
 end
