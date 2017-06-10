4c1f96c4c962bcf03630cec7a40bc3508a56a359
Dustin Essington <aetaric@gmail.com>
2017-06-10 12:57:19 -0400

    add basic sub detection

--- lib/twitch_helpers.rb
+++ lib/twitch_helpers.rb
@@ -17,6 +17,18 @@ def mod?(m)
   end
 end
 
+def sub?(m)
+  if m.tags["subscriber"].to_i == 1
+    return true
+  else
+    if chan_to_user(m) == m.user.nick
+      return true
+    else
+      return false
+    end
+  end
+end
+
 def channel_to_key(channel)
   return channel if channel.is_a?(Symbol)
   channel[/\w+/].to_sym
